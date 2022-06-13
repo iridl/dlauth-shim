fed9fab1ef13c93dd6639eab2841d361d7e2c419
Aaron Kaplan <aaron@iri.columbia.edu>
2022-06-13 17:40:45 -0400

    Minimal demo from stack overflow
    https://stackoverflow.com/questions/26313894/flask-login-using-linux-system-credentials

---
+++ miniauth.py
@@ -0,0 +1,41 @@
+from flask import Flask, session, redirect, url_for, escape, request
+from simplepam import authenticate
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    if 'username' in session:
+        return 'Logged in as %s' % escape(session['username'])
+    return 'You are not logged in'
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if authenticate(str(username), str(password)):
+            session['username'] = request.form['username']
+            return redirect(url_for('index'))
+        else:
+            return 'Invalid username/password'
+    return '''
+        <form action="" method="post">
+            <p><input type=text name=username>
+            <p><input type=password name=password>
+            <p><input type=submit value=Login>
+        </form>
+    '''
+
+@app.route('/logout')
+def logout():
+    # remove the username from the session if it's there
+    session.pop('username', None)
+    return redirect(url_for('index'))
+
+# set the secret key.  keep this really secret:
+app.secret_key = 'shh'
+
+if __name__ == '__main__':
+    app.run(debug='True', port=5001)
